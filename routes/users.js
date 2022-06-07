74b186f7ccfec08f897e5d4d9fa1d590dc4afc11
nuhman-bititude <nuhman@bititude.com>
2022-06-07 12:04:04 +0530

    Added express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,109 +1,34 @@
 var express = require("express");
 var router = express.Router();
-var path = require("path");
-var User = require("../models/userdb");
+var userController = require("../userController/controller");
+var { body } = require("express-validator");
 
-router.get("/user/create", function (req, res, next) {
-  // Create user, sendFile (HTML)
-  res.sendFile(path.resolve(__dirname, "../public/add_user.html"));
-});
+router.get("/user/create", userController.createUserForm);
 
-router.post("/users", async (req, res, next) => {
-  // POST METHOD FOR INSERTION OF USER INTO DB
-  firstname = req.body.firstName;
-  secondname = req.body.secondName;
-  email = req.body.email;
-  password = req.body.password;
-  try {
-    var users = await User.forge({
-      firstname: firstname,
-      secondname: secondname,
-      email: email,
-      password: password,
-    }).save();
-  } catch (err) {
-    console.error(err);
-    res.send(err);
-  }
-
-  res.send(
-    "success <br>  <a href='http://localhost:3000/users/'>Click here to see all users</>"
-  );
-});
-
-router.get("/users", (req, res, next) => {
-  // GET ALL USERS FROM DB
-  User.fetchAll().then((users) => {
-    res.json(users);
-  });
-});
-
-router.get("/users/:id", async (req, res, next) => {
-  // GET SPECIFIC USER WITH ID
-  id = req.params.id;
-  // console.log(typeof(id))
-  let user = await User.where({ id: parseInt(id) }).fetch({ require: true });
-  res.json(user);
-});
-
-router.get("/users/edit/:id", async (req, res, next) => {
-  // render page for editing
-  id = req.params.id;
-  try {
-    let user = await User.where({ id: parseInt(id) }).fetch({ require: true });
-    // console.log(user.attributes.firstname)
-    firstname = user.attributes.firstname;
-    secondname = user.attributes.secondname;
-    email = user.attributes.email;
-    password = user.attributes.password;
-    res.render("edit", {
-      firstname: firstname,
-      secondname: secondname,
-      email: email,
-      password: password,
-    });
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-router.patch(
-  // USER UPDATE WITH SPECIFC ID
-  "/users/edit/:id",
-  (req, res, next) => {
-    id = Number(req.params.id);
-    firstname = req.body.firstname;
-    secondname = req.body.secondname;
-    email = req.body.email;
-    password = req.body.password;
-    console.log(password);
-    try {
-      User.where({ id: id }).save(
-        {
-          firstname: firstname,
-          secondname: secondname,
-          email: email,
-          password: password,
-        },
-        { patch: true }
-      );
-      res.send("Updated User");
-    } catch (error) {
-      res.send(error);
-    }
-  }
+router.post(
+  "/users",
+  body("firstname").isLength({ min: 3 }),
+  body("secondname").isLength({ min: 2 }),
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  userController.createUser
 );
 
-router.get("/users/delete/:id"), // USER DELETE WITH SPECIFIC ID
-  async (req, res, next) => {
-    id = Number(req.params.id);
-    // console.log(id);
-    try {
-      let user = await User.where({ id: id }).destroy();
-      res.send("Deleted User");
-    } catch (error) {
-      res.send(error);
-    }
-  };
+router.get("/users", userController.fetchAll);
+
+router.get("/users/:id", userController.fetchOne);
+
+router.get("/users/edit/:id", userController.viewEdit);
+
+router.patch(
+  "/users/edit/:id",
+  body("firstname").isLength({ min: 3 }),
+  body("secondname").isLength({ min: 2 }),
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  userController.editUser
+);
+
+router.delete("/users/delete/:id", userController.deleteUser);
 
 module.exports = router;
---
+++ userController/controller.js
@@ -0,0 +1,139 @@
+const { body, validationResult } = require("express-validator");
+var path = require("path");
+var User = require("../models/userdb");
+
+exports.createUserForm = (req, res, next) => {
+  // Create user, sendFile (HTML)
+  res.sendFile(path.resolve(__dirname, "../public/add_user.html"));
+};
+
+exports.createUser = async (req, res, next) => {
+  // POST METHOD FOR INSERTION OF USER INTO DB
+  const errors = validationResult(req);
+  firstname = req.body.firstName;
+  secondname = req.body.secondName;
+  email = req.body.email;
+  password = req.body.password;
+  try {
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    var users = await User.forge({
+      firstname: firstname,
+      secondname: secondname,
+      email: email,
+      password: password,
+    }).save();
+  } catch (error) {
+    console.error(error);
+    if ((error.eerno = 1062)) {
+      res.send("User email Already exist");
+    } else {
+      res.send(error);
+    }
+  }
+
+  res.send(
+    "success <br>  <a href='http://localhost:3000/users/'>Click here to see all users</>"
+  );
+};
+
+exports.fetchAll = (req, res, next) => {
+  // GET ALL USERS FROM DB
+  try {
+    User.fetchAll().then((users) => {
+      res.json(users);
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.fetchOne = async (req, res, next) => {
+  // GET SPECIFIC USER WITH ID
+  try {
+    id = req.params.id;
+    // console.log(typeof(id))
+    let user = await User.where({ id: parseInt(id) }).fetch({ require: true });
+    res.json(user);
+  } catch (error) {
+    if (error.message == "EmptyResponse") {
+      res.send("No User Found");
+    } else {
+      res.send(error);
+    }
+  }
+};
+
+exports.viewEdit = async (req, res, next) => {
+  // render page for editing
+  id = req.params.id;
+  try {
+    let user = await User.where({ id: parseInt(id) }).fetch({ require: true });
+    // console.log(user.attributes.firstname)
+    firstname = user.attributes.firstname;
+    secondname = user.attributes.secondname;
+    email = user.attributes.email;
+    password = user.attributes.password;
+    res.render("edit", {
+      firstname: firstname,
+      secondname: secondname,
+      email: email,
+      password: password,
+    });
+  } catch (error) {
+    if (error.message == "EmptyResponse") {
+      res.send("No User Found");
+    } else {
+      res.send(error);
+    }
+  }
+};
+
+exports.editUser = (req, res, next) => {
+  // USER UPDATE WITH SPECIFC ID
+  const errors = validationResult(req);
+  id = Number(req.params.id);
+  firstname = req.body.firstname;
+  secondname = req.body.secondname;
+  email = req.body.email;
+  password = req.body.password;
+  console.log(password);
+  try {
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.where({ id: id }).save(
+      {
+        firstname: firstname,
+        secondname: secondname,
+        email: email,
+        password: password,
+      },
+      { patch: true }
+    );
+    res.send("Updated User");
+  } catch (error) {
+    if (error.message == "EmptyResponse") {
+      res.send("No User Found");
+    } else {
+      res.send(error);
+    }
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  // USER DELETE WITH SPECIFIC ID
+  id = Number(req.params.id);
+  // console.log(id);
+  try {
+    let user = await User.where({ id: id }).destroy();
+    res.send("Deleted User");
+  } catch (error) {
+    if (error.message == "EmptyResponse") {
+      res.send("No User Found");
+    } else {
+      res.send(error);
+    }
+  }
+};
